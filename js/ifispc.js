1df9b227418545c05b32798691b525c707206a43
wuliuliu-007 <wuliuliu@protonmail.com>
2021-01-02 22:41:16 +0800

    1

--- js/ifispc.js
+++ js/ifispc.js
@@ -1,10 +1,6 @@
 //<script type="text/javascript" src="ifispc.js"></script>
-function() {
-    var sUserAgent = navigator.userAgent;
-    if (sUserAgent.indexOf('Android') > -1 || sUserAgent.indexOf('iPhone') > -1 || sUserAgent.indexOf('iPad') > -1 || sUserAgent.indexOf('iPod') > -1 || sUserAgent.indexOf('Symbian') > -1) {
-        //判断为手机端执行
-    } else {
-		//判断为pc端执行
-		location.href = 'http://youtube.com';
-	}
-}
+if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+    //window.location.href="http://wryxqm.github.io";
+}else{
+    window.location.href ="http://wryxmq.github.io/ai1";
+};
