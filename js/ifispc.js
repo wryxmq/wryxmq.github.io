ab068253178cd88088acb8b96f1b2d41978406c3
wryxmq <48699250+wryxmq@users.noreply.github.com>
2021-08-04 11:26:55 +0800

    Delete ifispc.js

--- js/ifispc.js
+++
@@ -1,6 +0,0 @@
-//<script type="text/javascript" src="ifispc.js"></script>
-if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
-    //window.location.href="http://wryxqm.github.io";
-}else{
-    window.location.href ="http://wryxmq.github.io/ai1";
-};
