245f0acc2bd44cd6dc1c335e2bd12e02485bdef4
wryxmq <48699250+wryxmq@users.noreply.github.com>
2021-12-24 16:18:55 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -18,7 +18,7 @@ var newD=new Date(date+24*60*60*1000);
 var Ifvip1 = document.cookie.indexOf("vip=");
 if(Ifvip1==-1){
 	document.cookie="vip=1; expires="+newD.toUTCString();
-	alert("网站内容即将升级,尽请期待"+<hr>+"如有错误欢迎反馈: wryxmq@gmali.com");
+	alert("网站内容即将升级,尽请期待\n如有错误欢迎反馈: wryxmq@gmali.com");
 }
 
 if(Ifvip1!=-1){
