0234591e44136fb49b288208fb8f10618e6b0b88
wryxmq <48699250+wryxmq@users.noreply.github.com>
2023-11-04 12:08:59 +0800

    Update index.js

--- js/ads/index.js
+++ js/ads/index.js
@@ -1,6 +1,6 @@
 //修改顶部图片广告
 document.getElementById("ad_t1").setAttribute("href", "https://baidu.com");
-document.querySelector("#ad_t1 img").setAttribute("src", "images/hbfmgg.gif");
+document.querySelector("#ad_t1 img").setAttribute("src", "https://up.pcno.cn/view.php/517dfd4cda02c3efa7eb00e163b46a6b.png");
 
 //修改网站文字广告代码
 document.getElementById("ad_w1").innerHTML = '<a href="http://dpurl.cn/iPaYJitz" target="_blank" style="color: #ff0000;">【AD】美团外卖节,帮你吃更好</a>';
